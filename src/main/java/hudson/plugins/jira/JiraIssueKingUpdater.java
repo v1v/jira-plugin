55bc70f543b681b140f941620d9775ad3722c4fe
Victor Martinez <you@king.com>
2015-02-08 18:27:19 +0100

    Added new updater post-build action with some attributes

---
+++ src/main/java/hudson/plugins/jira/JiraIssueKingUpdater.java
@@ -0,0 +1,124 @@
+package hudson.plugins.jira;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.matrix.MatrixAggregatable;
+import hudson.matrix.MatrixAggregator;
+import hudson.matrix.MatrixBuild;
+import hudson.matrix.MatrixRun;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.model.Descriptor.FormException;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Publisher;
+import hudson.tasks.Recorder;
+import hudson.util.FormValidation;
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * Parses build changelog for JIRA issue IDs and then
+ * updates JIRA issues accordingly based on different attributes
+ *
+ * @author Victor Martinez
+ */
+public class JiraIssueKingUpdater extends Recorder implements MatrixAggregatable {
+	
+    private String assignee;
+    
+    @DataBoundConstructor
+    public JiraIssueKingUpdater(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+        // Don't do anything for individual matrix runs.
+        if (build instanceof MatrixRun) {
+            return true;
+        }
+        // TODO:
+        // https://developer.atlassian.com/jiradev/api-reference/jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-example-edit-issues#JIRARESTAPIExample-Editissues-Exampleofassigninganissuetouser"harry"
+        return Updater.perform(build, listener);
+    }
+
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.BUILD;
+    }
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return DESCRIPTOR;
+    }
+
+    @Extension
+    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+
+    public MatrixAggregator createAggregator(MatrixBuild build, Launcher launcher, BuildListener listener) {
+        return new MatrixAggregator(build, launcher, listener) {
+            @Override
+            public boolean endBuild() throws InterruptedException, IOException {
+                PrintStream logger = listener.getLogger();
+                logger.println("End of Matrix Build. Updating JIRA.");
+                // TODO:
+                // https://developer.atlassian.com/jiradev/api-reference/jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-example-edit-issues#JIRARESTAPIExample-Editissues-Exampleofassigninganissuetouser"harry"
+                return Updater.perform(this.build, this.listener);
+            }
+        };
+    }
+
+    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+        private DescriptorImpl() {
+            super(JiraIssueKingUpdater.class);
+        }
+
+        public FormValidation doCheckAssignee(@QueryParameter String value)
+                throws IOException {
+            if (value.length() == 0) {
+                return FormValidation.error("Please set the assigned user");
+            }
+            return FormValidation.ok();
+        }
+
+        @Override
+        public String getDisplayName() {
+            // Displayed in the publisher section
+            return Messages.JiraIssueKingUpdater_DisplayName();
+
+        }
+
+        @Override
+        public String getHelpFile() {
+            return "/plugin/jira/help-jira-issue-king-updater.html";
+        }
+
+        @Override
+        public Publisher newInstance(StaplerRequest req,
+                                                   JSONObject formData) throws FormException {
+            return req.bindJSON(JiraIssueKingUpdater.class, formData);
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+            return true;
+        }
+    }
+}
